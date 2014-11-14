32c8c31f41d43280d976966e0b05850c9028a609
rubenfm <ruben.fm@openmailbox.org>
2014-11-14 12:35:36 +0100

    Changes in the seed.rb and finished espacio.js javascipt. Starting to style it.

--- app/assets/javascripts/espacio.js
+++ app/assets/javascripts/espacio.js
@@ -15,32 +15,53 @@ function changeData(data){
 
   var cajaEventos = document.getElementsByClassName("cajaEventos")[0];
 
+  var listaEvento = document.createElement('ul');
+
   if (cajaEventos.firstChild){
-    console.log("dentro!");
     cajaEventos.innerHTML = "";
   };
  
   var aforoSala = document.createElement('li');
   aforoSala.textContent = "Aforo: " + data.capacidad + " asistentes.";
+
   var descSala = document.createElement('li');                        
-  descSala.textContent = "Descripción: " + data.descripcion;
+  descSala.textContent = "Descripción: " + data.descripcion + ".";
 
+  var wifiSala = document.createElement('li');                        
+  wifiSala.textContent = "Conexion Wifi disponible.";
 
+  var climaSala = document.createElement('li');                        
+  climaSala.textContent = "Sala climatizada.";
 
-  var listaEvento = document.createElement('ul')
+  if (data.equipo_informatico === true){
+    var audioSala = document.createElement('li');
+    audioSala.textContent = "Sala multimedia.";
+  }else
+  {
+    var audioSala = document.createElement('li');
+    audioSala.textContent = "Sala no multimedia.";
+  };
+
+  if (data.equipo_informatico === true){
+    var pcSala = document.createElement('li');
+    pcSala.textContent = "Sala Informatizada.";
+  }else
+  {
+    var pcSala = document.createElement('li');
+    pcSala.textContent = "Sala no informatizada.";
+  };
+
   listaEvento.textContent = data.nombre;
 
   listaEvento.appendChild(aforoSala);
   listaEvento.appendChild(descSala);
   cajaEventos.appendChild(listaEvento);
-
-
-
-  //cajaEventos.innerHTML = "<br>" + "<br>" + "<br>" + data.nombre + "<br>"+ "<br>" + "Aforo: " + data.capacidad + "<br>" + "Descripcion: " + data.descripcion;
+  listaEvento.appendChild(climaSala);
+  listaEvento.appendChild(wifiSala);
+  listaEvento.appendChild(pcSala);
 }
 
 function changeRoom(evt){
-  console.log('evento:', evt);
   var sala = evt.target.id;
   document.getElementById("mapa").src=espacios[sala];
   sala = evt.target.title;
